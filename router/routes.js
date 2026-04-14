490fab9d0e561cefefe7aa88d0e10770d7a2a503
EduardoToledo-WebDev <toledovaldezjesuseduardo@gmail.com>
2026-04-14 14:20:51 -0700

    Despliegue de la api a aws

--- router/routes.js
+++ router/routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getArticulos, registrarArticulo, entregarArticulo, eliminarArticulo, getAuditoria, editarArticulo } = require("../controllers/articulos");
+const { getArticulos, registrarArticulo, entregarArticulo, eliminarArticulo, getAuditoria, editarArticulo, upload } = require("../controllers/articulos");
 
 router.get("/articulos", getArticulos);
-router.post("/articulos", registrarArticulo);
+router.post("/articulos", upload.single("imagen"), registrarArticulo);
 router.put("/articulos/:id", entregarArticulo);
 router.delete("/articulos/:id", eliminarArticulo);
 router.put("/articulos/editar/:id", editarArticulo);
